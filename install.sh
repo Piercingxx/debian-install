daac1a74db4d5ba9227b32101f39721e8bbd8ae9
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-01-14 14:12:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# https://github.com/Piercing666
+
 # Check if Script is Run as Root
 if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user to run this script, please run sudo su then try again" 2>&1
