d200a1d6baccda70daedbee3f0ffafee2466f6a6
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-01-14 12:24:31 -0500

    Update sur-test.sh

--- sur-test.sh
+++ sur-test.sh
@@ -10,6 +10,20 @@ username=$(id -u -n 1000)
 builddir=$(pwd)
 
 
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+# Making dir
+cd $builddir
+mkdir -p /home/$username/.fonts
+mkdir -p /var/lib/usbmux/.config
+
+
+# Install Essential Programs part 1of2
+apt install nala 
+
 # Is this a Microsoft Surface Device?
 #!/bin/bash
 PS3='Is this A Microsoft Surface Device?: '
@@ -37,3 +51,19 @@ select fav in "${isSurface[@]}"; do
         *) echo "invalid option $REPLY";;
     esac
 done
+
+
+
+nala install tilix -y
+nala install gh -y
+
+
+apt update
+apt upgrade -y
+apt full-upgrade -y
+sudo apt install -f
+sudo dpkg --configure -a
+apt update
+apt upgrade
+flatpak update
+apt autoremove
