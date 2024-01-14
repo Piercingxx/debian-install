a382c51f744aba3682e0a012ecd03768c1b40ae0
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-01-14 08:10:51 -0500

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -21,9 +21,39 @@ mkdir -p /var/lib/usbmux/.config
 
 
 # Install Essential Programs part 1of2
-apt install nala -y
-nala install gnome-core -y
+apt install nala 
+nala install wget flatpak gnome-software-plugin-flatpak -y 
 
+
+
+# Add additional repositories
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+wget https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda-repo-debian12-12-3-local_12.3.2-545.23.08-1_amd64.deb
+dpkg -i cuda-repo-debian12-12-3-local_12.3.2-545.23.08-1_amd64.deb
+cp /var/cuda-repo-debian12-12-3-local/cuda-*-keyring.gpg /usr/share/keyrings/
+
+
+# Esure all repositories are up to date
+sudo rm /etc/apt/sources.list && sudo touch /etc/apt/sources.list && sudo chmod +rwx /etc/apt/sources.list && sudo printf "deb https://deb.debian.org/debian/ buster main contrib non-free
+deb http://security.debian.org/debian-security testing-security/updates main contrib non-free
+deb https://deb.debian.org/debian/ testing-updates main contrib non-free
+deb https://deb.debian.org/debian/ testing contrib non-free non-free-firmware main
+deb-src https://deb.debian.org/debian/ testing contrib non-free non-free-firmware main 
+deb-src https://deb.debian.org/debian/ testing-updates main contrib non-free" | sudo tee -a /etc/apt/sources.list
+
+
+nala update && upgrade -y
+apt full-upgrade -y
+sudo apt install -f
+sudo dpkg --configure -a
+apt update
+apt upgrade
+nala install --fix-broken
+flatpak update
+
+
+
+nala install gnome-core -y
 
 
 # Enable graphical login and change target from CLI to GUI
@@ -106,49 +136,41 @@ KeyringMode=shared
 WantedBy=multi-user.target" | sudo tee -a /lib/systemd/system/gdm.service
 
 
+nala update && upgrade -y
+apt full-upgrade -y
+sudo apt install -f
+sudo dpkg --configure -a
+
 
 # Actually enabling the admends just made
-
 systemctl enable gdm
 systemctl enable gdm3
 systemctl set-default graphical.target
 
-apt update
-apt upgrade -y
 
-# adding more essentials part 2of2
-nala install wget dpkg unzip flatpak gnome-software-plugin-flatpak -y 
+apt update && upgrade -y
+flatpak update -y
+flatpak upgrade -y
+apt autoremove -y
 
-# Add additional repositories
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-wget https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda-repo-debian12-12-3-local_12.3.2-545.23.08-1_amd64.deb
-dpkg -i cuda-repo-debian12-12-3-local_12.3.2-545.23.08-1_amd64.deb
-cp /var/cuda-repo-debian12-12-3-local/cuda-*-keyring.gpg /usr/share/keyrings/
-
-
-# Esure all repositories are up to date
-sudo rm /etc/apt/sources.list && sudo touch /etc/apt/sources.list && sudo chmod +rwx /etc/apt/sources.list && sudo printf "deb https://deb.debian.org/debian/ buster main contrib non-free
-deb http://security.debian.org/debian-security testing-security/updates main contrib non-free
-deb https://deb.debian.org/debian/ testing-updates main contrib non-free
-deb https://deb.debian.org/debian/ testing contrib non-free non-free-firmware main
-deb-src https://deb.debian.org/debian/ testing contrib non-free non-free-firmware main 
-deb-src https://deb.debian.org/debian/ testing-updates main contrib non-free" | sudo tee -a /etc/apt/sources.list
-
-
-apt update
-apt upgrade -y
-apt full-upgrade -y
-sudo apt install -f
-sudo dpkg --configure -a
-apt update
-apt upgrade --fix-broken
-flatpak update
-
-
-# Installing other less important but still important Programs y drivers
-nala install tilix gh pulseaudio pavucontrol build-essential -y --fix-broken
-nala install lua5.4 libxinerama-dev neofetch neovim nvidia-driver nvidia-opencl-icd cuda-toolkit-12-3 cuda-drivers -y --fix-broken
-nala install blender freecad inkscape gparted scribus librecad gnome-tweaks htop nvtop -y --fix-broken
+# Installing other less important but still important Programs and drivers
+nala install tilix -y --install-suggests
+nala install gh -y
+nala install build-essential -y
+nala install lua5.4 -y
+nala install neofetch -y
+nala install neovim -y
+nala install nvidia-driver -y
+nala install nvidia-opencl-icd -y
+nala install cuda-toolkit-12-3 cuda-drivers -y --install-suggests
+nala install blender -y
+nala install freecad  -y --install-suggests
+nala install inkscape  -y --install-suggests
+nala install gparted  -y --install-suggests
+nala install scribus  -y --install-suggests
+nala install librecad  -y --install-suggests
+nala install gnome-tweaks  -y --install-suggests
+nala install htop -y
 flatpak install flathub com.visualstudio.code -y
 flatpak install flathub md.obsidian.Obsidian -y
 flatpak install flathub com.synology.SynologyDrive -y
@@ -157,7 +179,9 @@ flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub com.obsproject.Studio -y
 flatpak install flathub com.mattjakeman.ExtensionManager -y
 flatpak install --user https://flathub.org/beta-repo/appstream/org.gimp.GIMP.flatpakref -y
+nala install nvtop -y 
 # the only app that I use and can not install via script is Davinci Resolve Studio
+
 
 
 # Installing fonts
@@ -170,10 +194,14 @@ unzip Meslo.zip -d /home/$username/.fonts
 mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
 chown $username:$username /home/$username/.fonts/*
 
+
+
 # Reloading Font
 fc-cache -vf
 # Removing zip Files
 rm ./FiraCode.zip ./Meslo.zip
+
+
 
 apt update
 apt upgrade -y
@@ -181,9 +209,13 @@ apt full-upgrade -y
 sudo apt install -f
 sudo dpkg --configure -a
 apt update
-apt upgrade --fix-broken
+apt upgrade
 flatpak update
 apt autoremove
 
+
+
 # Use nala
 bash scripts/usenala
+
+apt reboot
