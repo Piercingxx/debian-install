e543fd71e7923363f1434fa88a7768c83121e7c7
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-01-15 12:46:23 -0500

    Update and rename test.sh to 1.sh

--- 1.sh
+++ 1.sh
@@ -11,6 +11,7 @@ fi
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
+apt install nala -y
 
 sudo rm /etc/apt/sources.list && sudo touch /etc/apt/sources.list && sudo chmod +rwx /etc/apt/sources.list && sudo printf "deb https://deb.debian.org/debian/ stable main contrib non-free non-free-firmware
 deb http://security.debian.org/debian-security stable-security/updates main contrib non-free non-free-firmware
@@ -30,14 +31,14 @@ mkdir -p /var/lib/usbmux/.config
 
 
 # Install Essentials
-apt install nala wget flatpak gnome-software-plugin-flatpak -y
+apt install wget flatpak gnome-software-plugin-flatpak -y
 
 flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 apt update && upgrade -y
 apt full-upgrade -y
 sudo apt install -f
 flatpak update
-
 
 nala install gnome-core -y
 
@@ -126,77 +127,6 @@ WantedBy=multi-user.target" | sudo tee -a /lib/systemd/system/gdm.service
 systemctl enable gdm
 systemctl enable gdm3 --now
 sudo systemctl enable gdm3 --now
-
-nala update && upgrade -y
-apt full-upgrade -y
-sudo apt install -f
-sudo dpkg --configure -a
-
-
-# Installing other less important but still important Programs and drivers
-nala install tilix -y
-nala install build-essential -y
-nala install lua5.4 -y
-nala install neofetch -y
-nala install neovim -y
-nala install gparted -y
-nala install gnome-tweaks -y
-nala install htop -y
-apt update && upgrade -y
-flatpak install https://flathub.org/beta-repo/appstream/org.gimp.GIMP.flatpakref -y
-flatpak install flathub net.scribus.Scribus -y
-flatpak install flathub org.freecadweb.FreeCAD -y
-flatpak install flathub org.blender.Blender -y
-flatpak install flathub org.librecad.librecad -y
-flatpak install flathub org.inkscape.Inkscape -y
-flatpak install flathub com.dropbox.Client -y
-flatpak install flathub com.visualstudio.code -y
-flatpak install flathub md.obsidian.Obsidian -y
-flatpak install flathub com.synology.SynologyDrive -y
-flatpak install flathub com.valvesoftware.Steam -y
-flatpak install flathub com.discordapp.Discord -y
-flatpak install flathub com.obsproject.Studio -y
-flatpak install flathub com.mattjakeman.ExtensionManager -y
-nala install nvtop -y 
-apt update && upgrade -y
-nala install gh -y
-nala install cuda-toolkit-12-3 cuda-drivers -y
-apt update && upgrade -y
-nala install -y nvidia-kernel-open-dkms -y
-apt update && upgrade -y
-flatpak update -y
-
-
-# Installing fonts
-cd $builddir 
-nala install fonts-font-awesome fonts-noto-color-emoji -y
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip -d /home/$username/.fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-unzip Meslo.zip -d /home/$username/.fonts
-mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
-chown $username:$username /home/$username/.fonts/*
-
-
-# Reloading Font
-fc-cache -vf
-# Removing zip Files
-rm ./FiraCode.zip ./Meslo.zip
-
-
-
-
-
-
-
-
-
-# Video card drivers
-# nala install linux-headers-amd64 nvidia-driver firmware-misc-nonfree nvidia-installer-cleanup -y
-
-
-
-
 
 
 
