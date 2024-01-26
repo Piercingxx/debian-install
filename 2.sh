6dd6a5358bee2c130ea97eea6b3a6f532758c3eb
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-01-26 17:42:03 -0500

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -43,7 +43,7 @@ flatpak install flathub com.synology.SynologyDrive -y
 flatpak install flathub com.dropbox.Client -y
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub com.obsproject.Studio -y
-nala install firmware-iwlwifi -y
+nala install firmware-iwlwifi Bluetooth gnome-Bluetooth bluez bluez-tools pulseaudio-module-Bluetooth -y
 flatpak override com.synology.SynologyDrive --filesystem=host
 
 apt update && upgrade -y
