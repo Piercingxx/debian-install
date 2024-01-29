2ff4ae8da0c8d428ea0ee83780bb561e7d6d66eb
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-01-29 17:43:50 -0500

    Update Surface.sh

--- Surface.sh
+++ Surface.sh
@@ -19,10 +19,9 @@ apt update && upgrade -y
 wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
     | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/linux-surface.gpg
 
-echo "deb [arch=amd64] https://pkg.surfacelinux.com/debian release main" \
-	| sudo tee /etc/apt/sources.list.d/linux-surface.list
+echo "deb [arch=amd64] https://pkg.surfacelinux.com/debian release main" | sudo tee /etc/apt/sources.list.d/linux-surface.list
         
-apt update
+apt update && upgrade -y
 
 apt install linux-image-surface linux-headers-surface libwacom-surface iptsd -y
 
