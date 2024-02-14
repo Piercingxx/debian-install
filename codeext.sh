ab5f3271c61b68cc4e818d82f96db95c88d67b2b
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-02-14 16:56:33 -0500

    Create codeext.sh

---
+++ codeext.sh
@@ -0,0 +1,20 @@
+# Do not run as sudo
+
+code --install-extension sourcegraph.cody-ai
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension ritwickdey.LiveServer
+code --install-extension DaltonMenezes.aura-theme
+code --install-extension appulate.filewatcher
+code --install-extension jeanp413.open-remote-ssh
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension johnpapa.vscode-peacock
+code --install-extension kamikillerto.vscode-colorize
+code --install-extension oderwat.indent-rainbow
+code --install-extension sumneko.lua
+code --install-extension actboy168.lua-debug
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension SirTori.indenticator
+code --install-extension timonwong.shellcheck
+code --install-extension rogalmic.bash-debug
+code --install-extension mads-hartmann.bash-ide-vscode
+code --install-extension eamodio.gitlens
