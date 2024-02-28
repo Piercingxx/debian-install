f2c5f8809e17e5f9e7db82a020da18a80597b56e
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-02-27 19:37:22 -0500

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -9,30 +9,41 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 username=$(id -u -n 1000)
+echo "Username is: $username"
+
 builddir=$(pwd)
+echo "Build directory: $builddir"
 
+echo "Starting Script 3.sh"
 
+echo "Updating Repositiories"
 apt update && upgrade -y
+wait
 
+
+echo "Installing Several Nvidia Drivers, be patient this will take a while"
+
+# GreenWithEnvy used to control Nvidia fans and overclocking
 flatpak install flathub com.leinardi.gwe -y
 
 # Video card drivers
-
 wget https://developer.download.nvidia.com/compute/cuda/12.3.1/local_installers/cuda-repo-debian12-12-3-local_12.3.1-545.23.08-1_amd64.deb
 dpkg -i cuda-repo-debian12-12-3-local_12.3.1-545.23.08-1_amd64.deb
 cp /var/cuda-repo-debian12-12-3-local/cuda-*-keyring.gpg /usr/share/keyrings/
 
 
 apt update && upgrade -y
+wait
 
 nala install nvidia-kernel-open-dkms nvidia-driver nvidia-opencl-icd linux-headers-amd64 libnvidia-gl-535 libvulkan1 libvulkan1:i386 libnvidia-gl-535:i386 firmware-misc-nonfree nvidia-installer-cleanup -y
 
 apt update
+wait
 
 apt install cuda-toolkit-12-3 cuda-drivers nvidia-kernel-open-dkms -y
 
 flatpak install flathub org.freedesktop.Platform.GL.nvidia-545-29-06 -y
 
 apt update && upgrade -y 
-
+wait
 reboot
