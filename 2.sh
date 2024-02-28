4d6e6ce3e9ae5cd8674041233a326c35119ddf88
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-02-27 19:13:41 -0500

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -11,9 +11,18 @@ fi
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
+echo "Starting Script 2.sh"
+
+
+echo "Updating Repositiories"
+apt update && upgrade -y
+wait
+
 #Installing Priority Programs to setup while this script runs
+echo "Installing Priority Programs"
 nala install gnome-tweaks -y
 nala install nautilus -y
+nala install seahorse -y
 flatpak install flathub com.mattjakeman.ExtensionManager -y
 flatpak install flathub com.google.Chrome -y
 flatpak install flathub com.discordapp.Discord -y
@@ -27,7 +36,7 @@ nala install unzip -y
 nala install linux-headers-generic -y
 nala install lua5.4 -y
 # flatpak install flathub com.synology.SynologyDrive -y
-# I'm over the flatpak of synology...the .deb is soooo much better. 
+# I'm over the flatpak of synology...install the .deb its soooo much better. 
 
 flatpak install flathub com.visualstudio.code -y
 
@@ -38,6 +47,7 @@ flatpak install flathub com.visualstudio.code -y
 # rm -f packages.microsoft.gpg
 # apt install apt-transport-https
 # apt update
+# wait
 # apt install code -y
 
 # If this is your first time using VSCode then create an account and set it up with these extensions. 
@@ -62,31 +72,32 @@ flatpak install flathub com.visualstudio.code -y
 # code --install-extension johnpapa.vscode-peacock
 # code --install-extension jeanp413.open-remote-ssh
 # code --install-extension timonwong.shellcheck
- 
 
-
-
+echo "Installing Fonts"
 # Installing fonts
-cd $builddir 
+cd "$builddir" || exit
+
+
 nala install fonts-font-awesome fonts-noto-color-emoji -y
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip -d /home/$username/.fonts
+unzip FiraCode.zip -d /home/"$username"/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-unzip Meslo.zip -d /home/$username/.fonts
-mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
-chown $username:$username /home/$username/.fonts/*
+unzip Meslo.zip -d /home/"$username"/.fonts
+mv dotfonts/fontawesome/otfs/*.otf /home/"$username"/.fonts/
+chown "$username":"$username" /home/"$username"/.fonts/*
 
 # Reloading Font
 fc-cache -vf
+wait
 
 # Extensions - will need to be customized still
 # After full install dwl Alt+tab and User Themes - versions are not compatible between stable and testing branches.
-mkdir -p /home/$username/.local/share/gnome-shell/extensions
-cp -R dotlocal/share/gnome-shell/extensions/* /home/$username/.local/share/gnome-shell/extensions/
-chmod -R 777 /home/$username/.local/share/gnome-shell/extensions
+mkdir -p /home/"$username"/.local/share/gnome-shell/extensions
+cp -R dotlocal/share/gnome-shell/extensions/* /home/"$username"/.local/share/gnome-shell/extensions/
+chmod -R 777 /home/"$username"/.local/share/gnome-shell/extensions
 
 # Removing zip files and stuff
-rm ./FiraCode.zip ./Meslo.zip
+rm ./FiraCode.zip ./Meslo.zipk
 rm -r dotlocal
 re -r scripts
 
@@ -95,18 +106,24 @@ wget -cO- https://github.com/phisch/phinger-cursors/releases/latest/download/phi
 
 # Install Nordzy cursor
 git clone https://github.com/alvatip/Nordzy-cursors
-cd Nordzy-cursors
+cd Nordzy-cursors || exit
 ./install.sh
-cd $builddir
+cd "$builddir" || exit
 rm -rf Nordzy-cursors
 
 # icons
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 
-
+echo "Installing More Programs"
 # Installing other less important but still important Programs, drivers, etc
-flatpak install flathub org.libreoffice.LibreOffice -y
+
+# Install Steam
+wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
+sudo dpkg -i steam.deb
+rm steam.deb
+
 nala install gnome-calculator -y
+flatpak install flathub org.libreoffice.LibreOffice -y
 nala install rename -y
 nala install cups -y
 nala install util-linux -y
@@ -121,6 +138,7 @@ nala install x11-xserver-utils -y
 nala install dh-dkms -y
 nala install devscripts -y
 apt update && upgrade -y
+wait
 flatpak install https://flathub.org/beta-repo/appstream/org.gimp.GIMP.flatpakref -y
 flatpak install flathub org.gnome.SimpleScan -y
 flatpak install flathub net.scribus.Scribus -y
@@ -129,27 +147,33 @@ flatpak install flathub org.inkscape.Inkscape -y
 flatpak install flathub com.flashforge.FlashPrint -y
 flatpak install flathub com.obsproject.Studio -y
 flatpak install flathub com.usebottles.bottles -y
-apt update && upgrade -y
-flatpak update -y
+flatpak install flathub com.github.tchx84.Flatseal -y
+
 apt purge firefox -y
 apt purge firefox-esr -y
 
 
 # dependancy for DaVinci Resolve - have to install manually later, download from website
 nala install libfuse2 libglu1-mesa libxcb-composite0 libxcb-cursor0 libxcb-damage0 ocl-icd-libopencl1 libssl-dev ocl-icd-opencl-dev libpango-1.0-0-y
-cp /usr/lib/x86_64-linux-gnu/libglib-2.0.so.0 /opt/resolve/libs
-cd /opt/resolve/libs
-sudo mkdir /opt/resolve/libs/_disabled
-sudo mv libgio* libglib* libgmodule* libgobject* _disabled
-cd /
+# cp /usr/lib/x86_64-linux-gnu/libglib-2.0.so.0 /opt/resolve/libs
+# cd /opt/resolve/libs || exit
+# sudo mkdir /opt/resolve/libs/_disabled
+# sudo mv libgio* libglib* libgmodule* libgobject* _disabled
+# cd /
 
 
 apt update && upgrade -y
+wait
 apt full-upgrade -y
+wait
 apt install -f
+wait
 dpkg --configure -a
 apt install --fix-broken
+wait
 apt autoremove 
 apt update && upgrade -y
+wait
 flatpak update
+wait
 reboot
