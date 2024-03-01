77dde46316ca7208615a5473f35c02dbbe1007c0
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-02-29 20:29:26 -0500

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -22,6 +22,7 @@ wait
 
 
 echo "Installing Several Nvidia Drivers, be patient this will take a while"
+sleep 2
 
 # GreenWithEnvy used to control Nvidia fans and overclocking
 flatpak install flathub com.leinardi.gwe -y
@@ -30,14 +31,14 @@ flatpak install flathub com.leinardi.gwe -y
 wget https://developer.download.nvidia.com/compute/cuda/12.3.1/local_installers/cuda-repo-debian12-12-3-local_12.3.1-545.23.08-1_amd64.deb
 dpkg -i cuda-repo-debian12-12-3-local_12.3.1-545.23.08-1_amd64.deb
 cp /var/cuda-repo-debian12-12-3-local/cuda-*-keyring.gpg /usr/share/keyrings/
-
-
 apt update && upgrade -y
 wait
 
+echo "About half way there..."
+sleep .5
 nala install nvidia-kernel-open-dkms nvidia-driver nvidia-opencl-icd linux-headers-amd64 libnvidia-gl-535 libvulkan1 libvulkan1:i386 libnvidia-gl-535:i386 firmware-misc-nonfree nvidia-installer-cleanup -y
 
-apt update
+apt update && upgrade -y
 wait
 
 apt install cuda-toolkit-12-3 cuda-drivers nvidia-kernel-open-dkms -y
