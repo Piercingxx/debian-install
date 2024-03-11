0221447a6a7f4512ada7df98229ce1e6fbf93b47
Piercing666 <155384428+Piercing666@users.noreply.github.com>
2024-03-11 07:42:44 -0400

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -17,8 +17,9 @@ echo "Build directory: $builddir"
 echo "Starting Script 3.sh"
 
 # Checks for active network connection
-if [[ -n $(command -v nmcli) && $(nmcli -t -f STATE g) != connected ]]
-    then awk {print} <<< "Network connectivity is required to continue."; exit
+if [[ -n "$(command -v nmcli)" && "$(nmcli -t -f STATE g)" != connected ]]; then
+    awk '{print}' <<< "Network connectivity is required to continue."
+    exit
 fi
 
 echo "Updating Repositiories"
@@ -35,12 +36,13 @@ flatpak install flathub com.leinardi.gwe -y
 # Video card drivers
 wget https://developer.download.nvidia.com/compute/cuda/12.3.1/local_installers/cuda-repo-debian12-12-3-local_12.3.1-545.23.08-1_amd64.deb
 dpkg -i cuda-repo-debian12-12-3-local_12.3.1-545.23.08-1_amd64.deb
+wait
 cp /var/cuda-repo-debian12-12-3-local/cuda-*-keyring.gpg /usr/share/keyrings/
 apt update && upgrade -y
 wait
 
 echo "About half way there..."
-sleep .5
+sleep 2
 nala install nvidia-kernel-open-dkms nvidia-driver nvidia-opencl-icd linux-headers-amd64 libnvidia-gl-535 libvulkan1 libvulkan1:i386 libnvidia-gl-535:i386 firmware-misc-nonfree nvidia-installer-cleanup -y
 
 apt update && upgrade -y
@@ -48,10 +50,13 @@ wait
 
 apt install cuda-toolkit-12-3 cuda-drivers nvidia-kernel-open-dkms -y
 
+
 flatpak install flathub org.freedesktop.Platform.GL.nvidia-545-29-06 -y
 
 # necessary for steam
 apt install libgl1-nvidia-glvnd-glx:i386 -y
+
+rm cuda-repo-debian12-12-3-local_12.3.1-545.23.08-1_amd64.deb
 
 apt update && upgrade -y 
 wait
